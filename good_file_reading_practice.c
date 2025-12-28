d78387813f45396629a5c3a55032e544a007abe9
Mercy Nzambi Mbuthi <146635465+Mercy-Nzambi@users.noreply.github.com>
2025-12-28 20:10:39 +0300

    Adding if else statement to catch exceptions

---
+++ good_file_reading_practice.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *fptr;
+    fptr = fopen("lorem.txt", "r");
+
+    char myString[100];
+
+    if(fptr != NULL)
+    {
+        while(fgets(myString, 100, fptr))
+        {
+            printf("%s", myString);
+        }
+    }
+    else
+    {
+        printf("Not able to open the file.");
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
