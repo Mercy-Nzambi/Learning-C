a16661bcc0cb76dc0514eac8178346fe81864502
Mercy Nzambi Mbuthi <146635465+Mercy-Nzambi@users.noreply.github.com>
2025-12-28 20:28:38 +0300

    creating and writing in a file

--- writeFile.c
+++ writeFile.c
@@ -7,6 +7,14 @@ int main()
     //creating the file and assigning a mode of 'w' to write in the file
     fptr = fopen("learningC.txt", "w");
 
+    if(fptr == NULL)
+    {
+        printf("Error in creating file.");
+    }
+    else
+    {
+        printf("File created.");
+    }
     //writing something in the file we created
     fprintf(fptr, "Learning C is fun!");
     fclose(fptr);
